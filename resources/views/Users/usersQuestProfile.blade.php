55f2f320c1a3aaf89168edd6eed07041f009fa34
EkaterynaVolkova <volkova5005@gmail.com>
2017-12-15 17:22:27 +0200

    new

--- resources/views/Users/usersQuestProfile.blade.php
+++ resources/views/Users/usersQuestProfile.blade.php
@@ -10,117 +10,140 @@
     </header>
 
     <main>
-        <h1>{{Auth::user()->name}}</h1>
-        <h1>Текущий квест</h1>
-        <div class="column">
-            <div class="row">
-                <div class="text-center">Название</div>
-                <div class="text-center">Дата</div>
-                <div class="text-center">Время</div>
-                <div></div>
-            </div>
-            @foreach($quests as $key => $q)
-                @if($status[$key] == 1)
-                    <div class="row quest">
-                        <div class="text-center">{!! json_decode($q)->name !!}</div>
-                        <div class="text-center">{!! json_decode($q)->date !!}</div>
-                        <div class="text-center">{!! json_decode($q)->time !!}</div>
-                        <div>
-                            <button class="btn btn-link"><a href="" class="glyphicon glyphicon-th-list"
-                                                            onclick="openbox('id'); return false"></a></button>
-                        </div>
-                    </div>
-                    <div class="column task" id="id">
-                        <div class="row">
-                            <div class="text-center">Название</div>
-                            <div class="text-center">Описание</div>
-                            <div class="text-center">Длительность</div>
-                            <div class="text-center">Вес</div>
-                        </div>
-                        @foreach(json_decode($tasks[$key]) as $t)
-                            <div class="row">
-                                <div class="text-center">{!! $t->name !!}</div>
-                                <div class="text-center">{!! $t->description !!}</div>
-                                <div class="text-center">{!! $t->duration !!}</div>
-                                <div class="text-center">{!! $t->weight !!}</div>
+        <aside>
+            <div class="avatar"></div>
+            <p class="name">Имя: {{Auth::user()->name}}</p>
+            <p class="name">Возраст: {{Auth::user()->age}}</p>
+            <p class="name">Пол: {{Auth::user()->gender}}</p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <button class="btn btn-link link"><a href="" onclick="clear('all'); openbox('idTQ'); return false">Текущий квест</a>
+            </button>
+            <button class="btn btn-link link"><a href="" onclick="openbox('idFQ'); return false">Грядущие квесты</a>
+            </button>
+            <button class="btn btn-link link"><a href="" onclick="openbox('idAQ'); return false">Архив</a>
+            </button>
+        </aside>
+
+        <section class="section">
+
+            <div id="section_inner">
+
+            <div class="column all" id="idTQ">
+                <div class="row">
+                    <div class="text-center">Название</div>
+                    <div class="text-center">Дата</div>
+                    <div class="text-center">Время</div>
+                    <div></div>
+                </div>
+                @foreach($quests as $key => $q)
+                    @if($status[$key] == 1)
+                        <div class="row quest">
+                            <div class="text-center">{!! json_decode($q)->name !!}</div>
+                            <div class="text-center">{!! json_decode($q)->date !!}</div>
+                            <div class="text-center">{!! json_decode($q)->time !!}</div>
+                            <div>
+                                <button class="btn btn-link"><a href="" class="glyphicon glyphicon-th-list"
+                                                                onclick="openbox('id'); return false"></a></button>
                             </div>
-                        @endforeach
-                    </div>
-                @endif
-            @endforeach
-        </div>
-
-        <h1>Грядущие квесты</h1>
-
-        <div class="column">
-            <div class="row">
-                <div class="text-center">Название</div>
-                <div class="text-center">Дата</div>
-                <div class="text-center">Время</div>
-                <div></div>
-            </div>
-            @foreach($quests as $key => $q)
-                @if($status[$key] == 0)
-                    <div class="row quest">
-                        <div class="text-center">{!! json_decode($q)->name !!}</div>
-                        <div class="text-center">{!! json_decode($q)->date !!}</div>
-                        <div class="text-center">{!! json_decode($q)->time !!}</div>
-                        <div></div>
-                    </div>
-                @endif
-            @endforeach
-        </div>
-
-        <h1>Архив</h1>
-
-        <div class="column">
-            <div class="row">
-                <div class="text-center">Название</div>
-                <div class="text-center">Дата</div>
-                <div class="text-center">Время</div>
-                <div></div>
-            </div>
-            @foreach($quests as $key => $q)
-                @if($status[$key] == 2)
-                    <div class="row quest">
-                        <div class="text-center">{!! json_decode($q)->name !!}</div>
-                        <div class="text-center">{!! json_decode($q)->date !!}</div>
-                        <div class="text-center">{!! json_decode($q)->time !!}</div>
-                        <div>
-                            <button class="btn btn-link"><a href="" class="glyphicon glyphicon-th-list"
-                                                            onclick="openbox('id3'); return false"></a></button>
                         </div>
-                    </div>
-                    <div class="column task" id="id3">
-                        <div class="row">
-                            <div class="text-center">Название</div>
-                            <div class="text-center">Описание</div>
-                            <div class="text-center">Длительность</div>
-                            <div class="text-center">Вес</div>
-                        </div>
-                        @foreach(json_decode($tasks[$key]) as $t)
+                        <div class="column task" id="id">
                             <div class="row">
-                                <div class="text-center">{!! $t->name !!}</div>
-                                <div class="text-center">{!! $t->description !!}</div>
-                                <div class="text-center">{!! $t->duration !!}</div>
-                                <div class="text-center">{!! $t->weight !!}</div>
+                                <div class="text-center">Название</div>
+                                <div class="text-center">Описание</div>
+                                <div class="text-center">Длительность</div>
+                                <div class="text-center">Вес</div>
                             </div>
-                        @endforeach
-                    </div>
-                @endif
-            @endforeach
-        </div>
+                            @foreach(json_decode($tasks[$key]) as $t)
+                                <div class="row">
+                                    <div class="text-center">{!! $t->name !!}</div>
+                                    <div class="text-center">{!! $t->description !!}</div>
+                                    <div class="text-center">{!! $t->duration !!}</div>
+                                    <div class="text-center">{!! $t->weight !!}</div>
+                                </div>
+                            @endforeach
+                        </div>
+                    @endif
+                @endforeach
+            </div>
+
+
+
+            <div class="column all" id="idFQ">
+                <div class="row">
+                    <div class="text-center">Название</div>
+                    <div class="text-center">Дата</div>
+                    <div class="text-center">Время</div>
+                    <div></div>
+                </div>
+                @foreach($quests as $key => $q)
+                    @if($status[$key] == 0)
+                        <div class="row quest">
+                            <div class="text-center">{!! json_decode($q)->name !!}</div>
+                            <div class="text-center">{!! json_decode($q)->date !!}</div>
+                            <div class="text-center">{!! json_decode($q)->time !!}</div>
+                            <div></div>
+                        </div>
+                    @endif
+                @endforeach
+            </div>
+
+
+
+            <div class="column all" id="idAQ">
+                <div class="row">
+                    <div class="text-center">Название</div>
+                    <div class="text-center">Дата</div>
+                    <div class="text-center">Время</div>
+                    <div></div>
+                </div>
+                @foreach($quests as $key => $q)
+                    @if($status[$key] == 2)
+                        <div class="row quest">
+                            <div class="text-center">{!! json_decode($q)->name !!}</div>
+                            <div class="text-center">{!! json_decode($q)->date !!}</div>
+                            <div class="text-center">{!! json_decode($q)->time !!}</div>
+                            <div>
+                                <button class="btn btn-link"><a href="" class="glyphicon glyphicon-th-list"
+                                                                onclick="openbox('id3'); return false"></a></button>
+                            </div>
+                        </div>
+                        <div class="column task" id="id3">
+                            <div class="row">
+                                <div class="text-center">Название</div>
+                                <div class="text-center">Описание</div>
+                                <div class="text-center">Длительность</div>
+                                <div class="text-center">Вес</div>
+                            </div>
+                            @foreach(json_decode($tasks[$key]) as $t)
+                                <div class="row">
+                                    <div class="text-center">{!! $t->name !!}</div>
+                                    <div class="text-center">{!! $t->description !!}</div>
+                                    <div class="text-center">{!! $t->duration !!}</div>
+                                    <div class="text-center">{!! $t->weight !!}</div>
+                                </div>
+                            @endforeach
+                        </div>
+                    @endif
+                @endforeach
+            </div>
+
+            </div>
+        </section>
+
     </main>
 
     <footer></footer>
 
     <script type="text/javascript">
         function openbox(id) {
-            display = document.getElementById(id).style.display;
+               display = document.getElementById(id).style.display;
             if (display == 'none') {
-                document.getElementById(id).style.display = 'block';
+               document.getElementById(id).style.display = 'block';
             } else {
-                document.getElementById(id).style.display = 'none';
+              document.getElementById(id).style.display = 'none';
             }
         }
     </script>
