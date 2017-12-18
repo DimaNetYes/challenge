6605ae3058128094a2c552e3c874e785b894c912
Dima <realwindrunner@gmail.com>
2017-12-18 21:12:46 +0300

    added to admin Quest status field

--- resources/views/Admin/Quest/viewQuests.blade.php
+++ resources/views/Admin/Quest/viewQuests.blade.php
@@ -22,7 +22,7 @@
                                                       class="glyphicon glyphicon-plus"></a></submit>
             <?php
             echo "<table>";
-            echo "<tr><th>id</th><th>name</th><th>description</th><th>fullDescription</th><th>date</th><th>time</th></tr>";
+            echo "<tr><th>id</th><th>name</th><th>description</th><th>fullDescription</th><th>date</th><th>time</th><th>sts</th></tr>";
             foreach ($quests as $key => $value) {
             echo "<tr>";
             echo "<td> <div>" . $value->id . "</div> </td>";
@@ -31,6 +31,7 @@
             echo "<td class='description'> <div >" . $value->fullDescription . "</div> </td>";
             echo "<td class='date'> <div>" . $value->date . "</div> </td>";
             echo "<td> <div>" . $value->time . "</div> </td>";
+            echo "<td> <div>" . $value->status . "</div> </td>";
             echo "<td> ";
             ?>
             <submit class="btn btn-default btn-sm"><a href="{{route('editQuest', $value->id)}}"
