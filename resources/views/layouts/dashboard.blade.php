35d0248f59a3815508a25fe22b73a8013518d5de
EkaterynaVolkova <volkova5005@gmail.com>
2018-01-11 20:53:04 +0200

    start style

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="UTF-8">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     {{HTML::style('../bootstrap/bootstrap/css/bootstrap.css')}}
     @yield('style')
 </head>
