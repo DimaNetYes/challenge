cdb11113e1ab22f9247b556e0cc59960c9fb4655
Your Name <you@example.com>
2018-01-21 11:39:08 +0000

    n

--- storage/framework/views/5720b068987639251d81afc9acab0f6955d2754b.php
+++ storage/framework/views/5720b068987639251d81afc9acab0f6955d2754b.php
@@ -2,63 +2,89 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Quests</title>
-    <?php echo e(HTML::style('css/User/userViewQuests1.css')); ?>
-
     <?php echo e(HTML::style('css/User/userViewQuests.css')); ?>
+
+    <?php echo e(HTML::style('css/UserGeneral/headerNav.css')); ?>
 
     <?php echo e(HTML::script('js/uilang.js')); ?>
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 <?php $__env->stopSection(); ?>
 <?php $__env->startSection('content'); ?>
-<header>
-    <?php echo $__env->make('Users.General.headerNav', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
-</header>
+    <header>
+        <?php echo $__env->make('Users.General.headerNav', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+    </header>
 
-<main>
-    <div class="container-fluid">
-        <ul class="gallery-post-grid holder">
+    <main>
+        <div class="container-fluid">
+            
             <div class='row'>
 
                 <?php $__currentLoopData = $quests; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $key => $q): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
                     <?php if(count($quests) == 1): ?>
-                        <div class='col-xs-12 col-sm-12 col-md-12 dQ'><li class='quest' data-id='id-<?= $key ?>' data-type='illustration'>
+                        <div class='col-xs-12 col-sm-12 col-md-12 dQ'>
+                            <li class='quest' data-id='id-<?= $key ?>' data-type='illustration'>
                         <span class='gallery-hover-3col hidden-phone hidden-tablet'>
+                            <img src=<?php echo e($q->avatar); ?> class="thum" alt="image">
                             <span class='gallery-icons'>
-                                <a href='#' class='item-zoom-link lightbox' title='Просмотр' onclick='showDetails(this)' data-rel="<?php echo $q->name; ?>"></a>
-                                <a href='<?php echo e(route('more', ['id'=>$q->id])); ?>' class='item-details-link' title='Играть'></a>
+                                <a href='#' class='item-zoom-link lightbox' title='Просмотр' onclick='showDetails(this)'
+                                   data-rel="<?php echo $q->name; ?>"></a>
+                                <a href='<?php echo e(route('more', ['id'=>$q->id])); ?>' class='item-details-link '
+                                   title='Играть'></a>
                             </span>
                         </span>
-                        <span class='project-details'><a href='<?php echo e(route('more', ['id'=>$q->id])); ?>'><?php echo $q->name; ?></a><?php echo $q->description; ?><br><?php echo $q->date; ?>.</span>
-                    </li></div>
+                                <span class='project-details'><a
+                                            href='<?php echo e(route('more', ['id'=>$q->id])); ?>'><?php echo $q->name; ?></a><?php echo $q->description; ?>
+
+                                    <br><?php echo $q->date; ?></span>
+                            </li>
+                        </div>
                     <?php elseif(count($quests) < 3): ?>
-                        <div class='col-xs-12 col-sm-6 col-md-6 dQ'><li class='quest' data-id='id-<?= $key ?>' data-type='illustration'>
+                        <div class='col-xs-12 col-sm-6 col-md-6 dQ'>
+                            <li class='quest' data-id='id-<?= $key ?>' data-type='illustration'>
                         <span class='gallery-hover-3col hidden-phone hidden-tablet'>
+                            <img src=<?php echo e($q->avatar); ?> class="thum" alt="image">
                             <span class='gallery-icons'>
-                                <a href='#' class='item-zoom-link lightbox' title='Просмотр' onclick='showDetails(this)' data-rel="<?php echo $q->name; ?>"></a>
-                                <a href='<?php echo e(route('more', ['id'=>$q->id])); ?>' class='item-details-link' title='Играть'></a>
+                                                             <a href='#' class='item-zoom-link lightbox'
+                                                                title='Просмотр' onclick='showDetails(this)'
+                                                                data-rel="<?php echo $q->name; ?>"></a>
+                                <a href='<?php echo e(route('more', ['id'=>$q->id])); ?>' class='item-details-link'
+                                   title='Играть'></a>
                             </span>
                         </span>
-                        <span class='project-details'><a href='<?php echo e(route('more', ['id'=>$q->id])); ?>'><?php echo $q->name; ?></a><?php echo $q->description; ?><br><?php echo $q->date; ?>.</span>
-                    </li></div>
+                                <span class='project-details'><a href='<?php echo e(route('more', ['id'=>$q->id])); ?>'
+                                                                 class="name"><?php echo $q->name; ?></a><?php echo $q->description; ?>
+
+                                    <br><?php echo $q->date; ?></span>
+                            </li>
+                        </div>
                     <?php else: ?>
-                        <div class='col-xs-12 col-sm-6 col-md-4 dQ'><li class='quest' data-id='id-<?= $key ?>' data-type='illustration'>
+                        <div class='col-xs-12 col-sm-6 col-md-4 dQ'>
+                            <li class='quest' data-id='id-<?= $key ?>' data-type='illustration'>
                             <span class='gallery-hover-3col hidden-phone hidden-tablet'>
-                            <a href='#' class='pp'></a>
+                                <img src=<?php echo e($q->avatar); ?> class="thum" alt="image">
+                            <a href='#' class='pp'>  </a>
                                 <span class='gallery-icons'>
-                                    <a href='#' class='item-zoom-link lightbox' title='Просмотр' onclick='showDetails(this)' data-rel="<?php echo $q->name; ?>"></a>
-                                    <a href='<?php echo e(route('more', ['id'=>$q->id])); ?>' class='item-details-link' title='Играть'></a>
+                                                                      <a href='#' class='item-zoom-link lightbox'
+                                                                         title='Просмотр' onclick='showDetails(this)'
+                                                                         data-rel="<?php echo $q->name; ?>"></a>
+                                    <a href='<?php echo e(route('more', ['id'=>$q->id])); ?>' class='item-details-link'
+                                       title='Играть'></a>
                                 </span>
                             </span>
-                            <span class='project-details'><a href='<?php echo e(route('more', ['id'=>$q->id])); ?>'><?php echo $q->name; ?></a><?php echo $q->description; ?><br><?php echo $q->date; ?>.</span>
-                        </li></div>
+                                <span class='project-details'><a
+                                            href='<?php echo e(route('more', ['id'=>$q->id])); ?>'><?php echo $q->name; ?></a><?php echo $q->description; ?>
+
+                                    <br><?php echo $q->date; ?></span>
+                            </li>
+                        </div>
                     <?php endif; ?>
                 <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
 
             </div>
-        </ul>
-    </div>
-</main>
+            </ul>
+        </div>
+    </main>
     <footer>
         <div class="position">
             <div class="picture_center ">
@@ -76,7 +102,7 @@
         </div>
         <div class="bg"></div>
         <script>
-               // работает с data type в <a>
+            // работает с data type в <a>
             function showDetails(quest) {
                 var questType = quest.getAttribute("data-rel");
                 $('.questName').html(questType);
