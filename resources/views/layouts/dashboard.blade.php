f7b701196bed41579bebae27d317633f71e39281
EkaterynaVolkova <volkova5005@gmail.com>
2018-01-21 16:33:20 +0200

    img

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -5,16 +5,16 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>THE QUEST</title>
-    <link media="all" type="text/css" rel="stylesheet" href="/bootstrap/bootstrap/css/bootstrap.css">
-    @yield('style')
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+          integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+            integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+            crossorigin="anonymous"></script>
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    @yield('style')
 </head>
 <body>
-    @yield('content')
-    <script src="{{ url('../bootstrap/bootstrap/js/bootstrap.js')}}"></script>
-    <script src="{{ url('/js/main.js')}}"></script>
-    {{--{{HTML::script('../bootstrap/bootstrap/js/bootstrap.js')}}--}}
-    {{--{{HTML::script("http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js")}}--}}
-    {{--{{HTML::script('js/main.js')}}--}}
+@yield('content')
 </body>
 </html>
