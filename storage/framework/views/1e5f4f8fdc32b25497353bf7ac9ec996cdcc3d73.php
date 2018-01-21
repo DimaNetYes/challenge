3c29bf8637ffa1f5ff130c8c2fa46e91dcef4b63
EkaterynaVolkova <volkova5005@gmail.com>
2018-01-21 19:00:09 +0200

    Removed mylogfile.log

--- storage/framework/views/1e5f4f8fdc32b25497353bf7ac9ec996cdcc3d73.php
+++
@@ -1,7 +0,0 @@
-<nav class="leftNav">
-    <a href="<?php echo e(route('showUsers')); ?>">Пользователи</a>
-    <a href="<?php echo e(route('showQuests')); ?>">Квесты</a>
-    <a href="<?php echo e(route('showTasks')); ?>">Задания</a>
-    <a href="<?php echo e(route('showTeams')); ?>">Команды</a>
-    <a href="<?php echo e(route('resultQuest')); ?>">Результаты</a>
-</nav>
